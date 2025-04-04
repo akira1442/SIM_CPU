8b3eeb878177128a95e28965173c5cb19679adfd
Akira <145252650+akira1442@users.noreply.github.com>
2025-04-04 18:35:24 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -3,7 +3,8 @@
 Instruction* parse_data_instruction(const char* line, HashMap* memory_locations){
 
     int adresse = 0;
-    char* buffer = strtok(line, " ");
+    char* buffer = strdup(line);
+    buffer = strtok(buffer, " ");
 
     Instruction* res = (Instruction*)malloc(sizeof(Instruction));
 
@@ -19,7 +20,7 @@ Instruction* parse_data_instruction(const char* line, HashMap* memory_locations)
         //operand2 si c'est le troisieme "mot"
         else if (!res->operand2){
             res->operand2=buffer;
-            while (buffer = strtok(NULL, " ")){
+            while ((buffer = strtok(NULL, ","))){
                 // On verifie si on a pas atteint la fin de operand2 
                 adresse++;
             }
@@ -27,7 +28,7 @@ Instruction* parse_data_instruction(const char* line, HashMap* memory_locations)
         buffer = strtok(NULL, " ");
     }
 
-    HashMap_insert(memory_locations, res->mnemonic, adresse);
+    HashMap_insert(memory_locations, res->mnemonic, &adresse);
     return res;
 }
 
@@ -35,38 +36,36 @@ Instruction* parse_code_instruction(const char* line, HashMap* labels, int code_
     
     Instruction* res = (Instruction*)malloc(sizeof(Instruction));
 
-    if (fgets(buffer, sizeof(buffer), stdin)){
-        buffer[strcspn(buffer, '\n')] = '\0';
-    }
+    char* buffer = strdup(line);
 
     char* tmp = strtok(buffer, ", ");
     char*  etiquette = NULL;
 
     while (tmp != NULL){
         // verif si ':' est dans la ligne si oui regarder le 1er mot
-        if (strchr(buffer, ":") && (etiquette = strtok(tmp, ":"))){
-            HashMap_insert(labels, etiquette, code_count);
+        if (strchr(buffer, ':') && (etiquette = strtok(tmp, ":"))){
+            HashMap_insert(labels, etiquette, &code_count);
             tmp = strtok(NULL, ", ");
         }
         if (res->mnemonic == NULL){
-            strdup(res->mnemonic, tmp);
+            tmp=strdup(res->mnemonic);
         }else if (res->operand1 == NULL){
-            strdup(res->operand1, tmp);
+            tmp =strdup(res->operand1);
         }else if (res->operand2 == NULL){
-            strdup(res->operand2, tmp);
+            tmp=strdup(res->operand2);
         }
         tmp = strtok(NULL, ", ");
     }
     return res;
 }
 
-ParserResult* parse(const_char* filename){
+ParserResult* parse(const char* filename){
 
     FILE* f = fopen(filename, "r");
-    const char* line[255];
-    fgets(line, 255, f);
+    char line[255];
+    
     // boolean 1 si on pour si .DATA 0 si .CODE
-    int DorC;
+    int DorC = 1;
 
     ParserResult* new_parser = (ParserResult*) malloc(sizeof(ParserResult));
     new_parser->data_count = 0;
@@ -75,17 +74,17 @@ ParserResult* parse(const_char* filename){
     new_parser->code_instructions = (Instruction**) malloc(sizeof(Instruction*));
     new_parser->labels = (HashMap*)malloc(sizeof(HashMap));
 
-    while (line != NULL){
-        if (strcmp(line, ".DATA") == 0){
-            new_parser->data_instructions=parse_data_instruction(line, new_parser->memory_locations);
-            new_parser->data_count++;                
-        }else if (strcmp(line, ".CODE") == 0){        
-            while(strcmp(line, ".DATA") == 1){
-                new_parser->code_instructions=parse_code_instruction(line, new_parser->labels,new_parser->code_count);
-                new_parser->code_count++;
-            }
+    while (fgets(line, 255, f)){
+        if ((strcmp(line, ".DATA") == 0) && DorC){
+            new_parser->data_instructions[new_parser->data_count] = (parse_data_instruction(line, new_parser->memory_locations));
+            new_parser->data_count++;   
+            DorC = 1;             
+        }else if ((strcmp(line, ".CODE") == 0) && !DorC){        
+            new_parser->code_instructions[new_parser->code_count] = (parse_code_instruction(line, new_parser->labels,new_parser->code_count));
+            new_parser->code_count++;
+            DorC = 0;
         }
-        fgets(line, 255, f);
+        //fgets(line, 255, f);
     }
     
     fclose(f);
@@ -116,4 +115,36 @@ void free_parser_result(ParserResult* result){
             }
         }
     }
+}
+
+void afficher_instructions(Instruction** instructions, int count) {
+    for (int i = 0; i < count; ++i) {
+        if (instructions[i] != NULL) {
+            printf("Instruction %d: Mnemonic: %s, Operand1: %s, Operand2: %s\n", 
+                    i, instructions[i]->mnemonic, instructions[i]->operand1, instructions[i]->operand2);
+        }
+    }
+}
+
+void afficher_parser_result(ParserResult* result) {
+    if (result == NULL) {
+        printf("ParserResult est NULL.\n");
+        return;
+    }
+
+    printf("=== ParserResult ===\n");
+
+    printf("\nInstructions .DATA:\n");
+    afficher_instructions(result->data_instructions, result->data_count);
+
+    printf("\nInstructions .CODE:\n");
+    afficher_instructions(result->code_instructions, result->code_count);
+
+    printf("\nLabels (indices dans code_instructions):\n");
+    afficher_hashmap(result->labels);
+
+    printf("\nEmplacements mémoire (noms de variables -> adresse mémoire):\n");
+    afficher_hashmap(result->memory_locations);
+    
+    printf("\n=====================\n");
 }
